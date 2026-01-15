a8b2d4ea72bd679281e3e341122da0e35f803001
maxsudbekjon <toshpulatovm73@gmail.com>
2026-01-15 14:30:23 +0500

    a

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,9 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path,re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
+from django.views.static import serve
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
@@ -17,3 +17,8 @@ urlpatterns = i18n_patterns(
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        ]
