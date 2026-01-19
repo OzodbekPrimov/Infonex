6f0a592477a9bbf5554ba8685be3812214c4b4d7
ChoriyevJamshid <jamshidchoriyev709@gmail.com>
2026-01-19 15:09:55 +0500

    .

--- app/views.py
+++ app/views.py
@@ -305,7 +305,7 @@ class TeamDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 @extend_schema(tags=["Service"])
 class ServiceListCreateView(generics.ListCreateAPIView):
-    queryset = Service.objects.all().order_by("order")
+    queryset = Service.objects.all()
     serializer_class = ServiceSerializer
     permission_classes = [IsSuperuserOrReadOnly]
 
